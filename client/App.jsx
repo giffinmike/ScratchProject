49f2fa014ccbeb8f920db3309e9a7ab610765ba1
david palmer <dwppalmer95@gmail.com>
2022-04-04 21:15:38 -0500

    created signInSide

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TestComp from "./components/muiTest.jsx";
+import SignInSide from "./components/SignInSide.js";
 
 export default function App() {
-  return <TestComp/>;
+  return <SignInSide/>;
 }
