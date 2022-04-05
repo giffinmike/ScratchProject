d8cf0a77a5feaf86c39e756b710d31d271a0077b
david palmer <dwppalmer95@gmail.com>
2022-04-04 21:50:38 -0500

    added theme to sign in code

--- client/components/SignInSide.js
+++ client/components/SignInSide.js
@@ -14,7 +14,16 @@ import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { green, purple } from '@mui/material/colors';
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 export default function SignInSide() {
   const handleSubmit = (event) => {
