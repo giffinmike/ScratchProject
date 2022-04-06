0300caca75c42a4d101938d3b1049d0fdd5f00bd
Adrian Karnani <adriankarnani@gmail.com>
2022-04-05 23:23:40 -0400

    updated API calls, sortNews logic completed

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 const db = require('../models/newsModel');
+const biasData = require('../allSidesData/allsides')
 
 const options = {
 	method: 'GET',
@@ -11,128 +12,104 @@ const options = {
 
 const newsController = {};
 
-function retrieveDate(i){
+// function retrieveDate(i){
   //&before=2022-03-31&after=2022-04-01
-  var date = new Date();
-  var yesterday = new Date(date.getTime());
-  yesterday.setDate(date.getDate() - i);
-  yesterday = JSON.stringify(yesterday).slice(1,11);
-  const returnString = `&before=${yesterday}`;
-  return returnString;
-}
-
-// newsController.breakingNews = (req, res, next) => {
-//   const articlesArray = [];
-//   function retrieveArticles (query) {
-//     fetch(`https://google-news1.p.rapidapi.com/search?q=Politics&country=US&lang=en&limit=50${query}`, options)
-// 	  .then(response => response.json())
-// 	  .then(response => {
-//       for(let i = 0; i < response.articles.length; i++){
-//         articlesArray.push(response.articles[i])
-//       }
-//       res.locals.articles = articlesArray;
-//       console.log(res.locals.articles.length);
-//     })
-// 	  .catch(err => next(err));
-//   }
-//   const numberOfFetchCalls = 4;
-// 	for(let i = 0; i < numberOfFetchCalls; i++){
-//     const beforeQuery = retrieveDate(i);
-//     retrieveArticles(beforeQuery);
-//   }
-//   return next();
+//   var date = new Date();
+//   var yesterday = new Date(date.getTime());
+//   yesterday.setDate(date.getDate() - i);
+//   yesterday = JSON.stringify(yesterday).slice(1,11);
+//   const returnString = `&before=${yesterday}`;
+//   return returnString;
 // }
 
 newsController.breakingNews = (req, res, next) => {
   let articlesArray = [];
-  fetch(`https://google-news1.p.rapidapi.com/search?q=Politics&country=US&lang=en&limit=50&when=30d&media=true${retrieveDate(0)}`, options)
+  fetch('https://google-news1.p.rapidapi.com/top-headlines?country=US&lang=en&limit=50&media=true', options)
 	.then(response => response.json())
 	.then(response => {
     for(let i = 0; i < response.articles.length; i++){
       articlesArray.push(response.articles[i])
     }
-  })
-  .then(
-    fetch(`https://google-news1.p.rapidapi.com/search?q=Politics&country=US&lang=en&limit=50&when=30d&media=true${retrieveDate(1)}`, options)
+    console.log('fetch call number 1')
+    fetch('https://google-news1.p.rapidapi.com/topic-headlines?topic=NATION&country=WORLD&lang=en&limit=50&media=true', options)
 	  .then(response => response.json())
 	  .then(response => {
     for(let i = 0; i < response.articles.length; i++){
       articlesArray.push(response.articles[i])
     }
-    })
-  )
-  .then(
-    fetch(`https://google-news1.p.rapidapi.com/search?q=Politics&country=US&lang=en&limit=50&when=30d&media=true${retrieveDate(2)}`, options)
-	  .then(response => response.json())
-	  .then(response => {
-    for(let i = 0; i < response.articles.length; i++){
-      articlesArray.push(response.articles[i])
+    console.log('fetch call number 2')
+    fetch('https://google-news1.p.rapidapi.com/search?q=Democrat&country=US&lang=en&limit=50&when=30d&media=true', options)
+      .then(response => response.json())
+      .then(response => {
+      for(let i = 0; i < response.articles.length; i++){
+        articlesArray.push(response.articles[i])
+      }
+      console.log('fetch call number 3')
+      fetch('https://google-news1.p.rapidapi.com/search?q=Republican&country=US&lang=en&limit=50&when=30d&media=true', options)
+          .then(response => response.json())
+          .then(response => {
+          for(let i = 0; i < response.articles.length; i++){
+            articlesArray.push(response.articles[i])
+          }
+          console.log('fetch call number 4')
+          res.locals.articles = articlesArray;
+          return next();
+          })
+      })
     }
-    })
-  )
-  .then(
-    fetch(`https://google-news1.p.rapidapi.com/search?q=Politics&country=US&lang=en&limit=50&when=30d&media=true${retrieveDate(3)}`, options)
-	  .then(response => response.json())
-	  .then(response => {
-    for(let i = 0; i < response.articles.length; i++){
-      articlesArray.push(response.articles[i])
-    }
-    res.locals.articles = articlesArray;
-    return next();
-    })
   )
 	.catch(err => next(err));
-}
-
-// newsController.breakingNews = (req, res, next) => {
-  
-//   async function retrieveArticles (query) {
-    
-//     let response = await fetch(`https://google-news1.p.rapidapi.com/search?q=Politics&country=US&lang=en&limit=50${query}`, options);
-//     response = response.json();
-//     console.log(response);
-//     // res.locals.articles = [...response.articles];
-// 	  // console.log(res.locals.articles.length);
-//   }
-
-//   const numberOfFetchCalls = 4;
-//   for(let i = 0; i < numberOfFetchCalls; i++){
-//     const beforeQuery = retrieveDate(i);
-//     retrieveArticles(beforeQuery);
-//   }
-
-//   return next();
-// }
+  })
+};
 
 newsController.sortNews = (req, res, next) => {
-  console.log("line 26")
-  console.log(res.locals.articles[0])
-  //SELECT bias FROM allsides WHERE name={theName};
-  //const speciesId = [`${req.query.id}`];
-  // const text = `SELECT species.classification, species.average_height, species.average_lifespan, 
-  // species.language, species.name, planets.name homeworld
-  // FROM species LEFT JOIN planets ON species.homeworld_id = planets._id WHERE species._id = $1`;
   
-  // db.query(text, speciesId)
-  // .then((data) => {
-  //   res.locals.species = data.rows[0];
-  //   return next();
-  // })
-  for(i = 0; i < 2; i++){
+  const returnArray = [[],[],[],[],[]];
+  res.locals.articles.forEach ((el, i, arr) => {
+    
     let publication;
-    res.locals.articles[i].source.title.startsWith('The ') ? 
-      publication = res.locals.articles[i].source.title.slice(4) : 
-      publication = res.locals.articles[i].source.title;
-    console.log(publication);
-    const text = `SELECT bias FROM allsides_data WHERE name = '${publication}'`;
-    console.log(text)
-    db.query(text)
-      .then((data) => {
-        // console.log(data);
-        // console.log(data.rows);
-        console.log(data.rows[0]);
-      })
-  }
+    el.source.title.startsWith('The ') ? 
+      publication = el.source.title.slice(4).toLowerCase() : 
+      publication = el.source.title.toLowerCase();
+
+    for(let i = 0; i <= biasData.length; i++){
+      if (i < biasData.length) {
+    
+        if(biasData[i].name.toLowerCase().includes(publication)) {
+
+        switch(biasData[i].bias) {
+          case 'left':
+            // console.log('left -->', publication)
+            returnArray[0].push(el);
+            break;
+          case 'left-center':
+            // console.log('left-center -->', publication)  
+            returnArray[1].push(el);
+            break;
+          case 'center':
+            // console.log('center -->', publication)  
+            returnArray[2].push(el);
+            break;
+          case 'right-center':
+            // console.log('right-center -->', publication)  
+            returnArray[3].push(el);
+            break;
+          case 'right':
+          //  console.log('right -->', publication)
+            returnArray[4].push(el);
+            break;
+        };
+        break;
+      }} 
+      // else {
+      //   console.log('NO BIAS FOUND -->', publication)
+      // }
+    };
+  });
+  console.log(returnArray[0].length, returnArray[1].length, returnArray[2].length, returnArray[3].length, returnArray[4].length);
+  // for(let i = 0; i < returnArray.length; i++){
+
+  // }
 }
 
 newsController.searchNews = (req, res, next) => {
