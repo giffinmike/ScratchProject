c4f593cc211a58e850010da48f673c6a19749a65
Matthew Birnholtz <mattbirn93@gmail.com>
2022-04-05 20:34:08 -0400

    updatedBranch

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -72,7 +72,7 @@ export default function SearchAppBar() {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            Hoot News
+            HOOT
           </Typography>
           <Search>
             <SearchIconWrapper>
