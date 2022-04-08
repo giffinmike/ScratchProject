2e1d92e6eeea5a2f35c300902c27dce3378d46dd
david palmer <dwppalmer95@gmail.com>
2022-04-07 21:26:36 -0400

    added encryption to auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
-const { query } = require('express');
 const db = require('../models/newsModel.js');
 const queries = require('./queries.js');
+const bcrypt = require('bcrypt');
 
 const userController = {};
 
@@ -8,24 +8,38 @@ userController.createUser = (req, res, next) => {
   const { username, password } = req.body
   if (username && password){
 
-    db.query(queries.postUser, [username, password], (queryErr, queryRes) => {
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, function(hashErr, hash) {
 
-      if (queryErr) {
-        const parsedQueryErr = `${queryErr}`;
-        const message = parsedQueryErr.includes('duplicate key') ? 
-          'Username already exists. Please try again.' :
-          'A query error occured posting user data. See console.';
+      if (hashErr) {
         const err = {
-          log: `Query error in postUser: ${parsedQueryErr}`,
+          log: `Password hashing error. At userController.createUser: ${hashErr}`,
           status: 500,
-          message
-        }
+          message: 'Error creating user info. See console.'
+        };
         return next(err);
-      }
-      console.log('user data post success');
-      res.locals = { username, password };
+      };
 
-      return next(); 
+      db.query(queries.postUser, [username, hash], (queryErr, queryRes) => {
+
+        if (queryErr) {
+          const parsedQueryErr = `${queryErr}`;
+          const message = parsedQueryErr.includes('duplicate key') ? 
+            'Username already exists. Please try again.' :
+            'A query error occured posting user data. See console.';
+          const err = {
+            log: `Query error in postUser: ${parsedQueryErr}`,
+            status: 500,
+            message
+          }
+          return next(err);
+        }
+        
+        res.locals = { username, hash, userCreated: true };
+  
+        return next(); 
+  
+      });
 
     });
 
@@ -38,21 +52,36 @@ userController.verifyUser = (req, res, next) => {
   db.query(queries.getUser, [username], (queryErr, queryRes) => {
 
     if (queryErr) {
-      const parsedQueryErr = `${queryErr}`;
       const err = {
-        log: `Query error in userController.verifyUser: ${parsedQueryErr}`,
+        log: `Query error in userController.verifyUser: ${queryErr}`,
         status: 500,
         message: 'Error getting user data. See console.'
       }
       return next(err);
     }
 
-    const loginSuccess = queryRes.rows.length !== 0 && 
-      queryRes.rows[0].password === password;
+    if (queryRes.rows.length === 0) {
+      res.locals.loginSuccess = false;
+      next();
+    }
 
-    res.locals.loginSuccess = loginSuccess;
-    
-    next();
+    const hash = queryRes.rows[0].password;
+    bcrypt.compare(password, hash, function(hashErr, result) {
+
+      if (hashErr) {
+        const err = {
+          log: `Password hashing error. At userController.verifyUser: ${hashErr}`,
+          status: 500,
+          message: 'Error verifying user info. See console.'
+        };
+        return next(err);
+      };
+
+      res.locals.loginSuccess = result;
+      next();
+
+    });
+
   })
 }
 
